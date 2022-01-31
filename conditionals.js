f65456525d930a5efe75920679c4174423c29517
annemiekeSegersNovi <93577704+annemiekeSegersNovi@users.noreply.github.com>
2022-01-31 14:55:59 +0100

    Conditionals.js ingevuld

--- conditionals.js
+++ conditionals.js
@@ -8,24 +8,28 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
 // ==========================================
-
+const isTenBiggerThenTwenty = 10 => 20;
+console.log(isTenBiggerThenTwenty);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
 // ==========================================
-
+let tenIsNotEleven = 10 !== 11;
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false wanneer gelogd
 // ==========================================
-
+let sweetIsNotJuicy= "zoet" === "sappig";
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
 // 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!' wanneer gelogd
 // ==========================================
-
+rapportCijfer = 9;
+if(rapportCijfer => 8;) {
+  console.log('Cum laude!');
+}
 
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
@@ -36,3 +40,10 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 
 const isRaining = false;
 const temperature = 23;
+
+if (!isRaining && temperature => 21) {
+     console.log("Je kunt een korte broek aan!");
+}
+else {
+    console.log("Doe een jas aan!");
+}
